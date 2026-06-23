43da7a66e5fe43c8dd2774beeb69f9de9238d695
limendusk <cairuan.ziugin@gmail.com>
2026-06-22 22:19:48 -0400

    Create function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!")
+function getComputerChoice() {
+    let computer_choice;
+
+    let random_number = Math.random()*3;
+
+    // Set the value of computer_choice based on the value of random_number.
+    if (random_number < 1.0) {
+        computer_choice = "rock";
+    } else if (random_number < 2.0) {
+        computer_choice = "paper";
+    } else {
+        computer_choice = "scissors";
+    }
+
+    return computer_choice;
+}
