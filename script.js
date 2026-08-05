9be82b1501a7d2ddeb5665094e3823ccf0e89365
limendusk <cairuan.ziugin@gmail.com>
2026-08-05 19:39:03 -0400

    Add logic to display choice, outcome, and scores
    After each button press, the player and computer's choice,
    outcome of the round, and running scores are displayed underneath.

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let human_score = 0;
+let computer_score = 0;
+
+const result_container = document.querySelector(".result-container");
+
+const [choice_text, round_outcome, running_score] = Array.from({ length: 3 }, () => document.createElement("p"));
+result_container.append(choice_text, round_outcome, running_score);
+
+const buttons = document.querySelectorAll(".choice");
+buttons.forEach((button) => {
+    button.addEventListener("click", 
+        () => playRound(button.textContent.toLowerCase(), getComputerChoice()));
+});
+
 function getComputerChoice() {
     let computer_choice;
 
@@ -15,53 +29,41 @@ function getComputerChoice() {
     return computer_choice;
 }
 
-let human_score = 0;
-let computer_score = 0;
-
 function playRound(human_choice, computer_choice) {
     
-    console.log(`Your choice: ${human_choice}, Computer choice: ${computer_choice}`);
+    choice_text.textContent = `Your choice: ${human_choice}, Computer choice: ${computer_choice}`;
 
     if (human_choice === "rock") {
         if (computer_choice === "rock") {
-            console.log("It's a draw!");
+            round_outcome.textContent = "It's a draw!";
         } else if (computer_choice === "paper") {
-            console.log("You lose! Paper beats Rock");
+            round_outcome.textContent = "You lose! Paper beats Rock";
             computer_score++;
         } else {    // computer_choice === "scissors"
-            console.log("You win! Rock beats Scissors");
+            round_outcome.textContent = "You win! Rock beats Scissors";
             human_score++;
         }
     } else if (human_choice === "paper") {
         if (computer_choice === "rock") {
-            console.log("You win! Paper beats Rock");
+            round_outcome.textContent = "You win! Paper beats Rock";
             human_score++;
         } else if (computer_choice === "paper") {
-            console.log("It's a draw!");
+            round_outcome.textContent = "It's a draw!";
         } else {
-            console.log("You lose! Scissors beats Paper")
+            round_outcome.textContent = "You lose! Scissors beats Paper";
             computer_score++;
         }
     } else if (human_choice === "scissors") {
         if (computer_choice === "rock") {
-            console.log("You lose! Rock beats Scissors");
+            round_outcome.textContent = "You lose! Rock beats Scissors";
             computer_score++;
         } else if (computer_choice === "paper") {
-            console.log("You win! Scissors beats Paper");
+            round_outcome.textContent = "You win! Scissors beats Paper";
             human_score++;
         } else {
-            console.log("It's a draw!")
+            round_outcome.textContent = "It's a draw!"
         }
-    } else {
-        console.log("Invalid choice");
     }
 
-    console.log(`Your score: ${human_score}, Computer score: ${computer_score}`);
+    running_score.textContent = `Your score: ${human_score}, Computer score: ${computer_score}`;
 }
-
-const buttons = document.querySelectorAll(".choice");
-
-buttons.forEach((button) => {
-    button.addEventListener("click", 
-        () => playRound(button.textContent.toLowerCase(), getComputerChoice()));
-})
