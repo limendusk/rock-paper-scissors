debc4d2fabf44c4607c9332e67fa41cd962044e2
limendusk <cairuan.ziugin@gmail.com>
2026-08-10 19:18:55 -0400

    Add logic to play a game of 5 rounds
    Upon clicking on any of the buttons, the game will start and a round
    will be played. When 5 rounds are played, the results of the game will
    be announced. Clicking on any of the buttons after 5 rounds will restart
    the game.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 let human_score = 0;
 let computer_score = 0;
+let rounds_played = 0;
 
 const result_container = document.querySelector(".result-container");
 
-const [choice_text, round_outcome, running_score] = Array.from({ length: 3 }, () => document.createElement("p"));
-result_container.append(choice_text, round_outcome, running_score);
+const [choice_text, round_outcome, running_score, game_outcome] = Array.from({ length: 4 }, () => document.createElement("p"));
+result_container.append(choice_text, round_outcome, running_score, game_outcome);
 
 const buttons = document.querySelectorAll(".choice");
 buttons.forEach((button) => {
@@ -30,40 +31,62 @@ function getComputerChoice() {
 }
 
 function playRound(human_choice, computer_choice) {
-    
+
+    rounds_played++;
     choice_text.textContent = `Your choice: ${human_choice}, Computer choice: ${computer_choice}`;
+    round_outcome.textContent = "Round outcome: ";
+    game_outcome.textContent = "";
+
+    let result;
 
     if (human_choice === "rock") {
         if (computer_choice === "rock") {
-            round_outcome.textContent = "It's a draw!";
+            result = "It's a draw!";
         } else if (computer_choice === "paper") {
-            round_outcome.textContent = "You lose! Paper beats Rock";
+            result = "You lose! Paper beats Rock";
             computer_score++;
         } else {    // computer_choice === "scissors"
-            round_outcome.textContent = "You win! Rock beats Scissors";
+            result = "You win! Rock beats Scissors";
             human_score++;
         }
     } else if (human_choice === "paper") {
         if (computer_choice === "rock") {
-            round_outcome.textContent = "You win! Paper beats Rock";
+            result = "You win! Paper beats Rock";
             human_score++;
         } else if (computer_choice === "paper") {
-            round_outcome.textContent = "It's a draw!";
+            result = "It's a draw!";
         } else {
-            round_outcome.textContent = "You lose! Scissors beats Paper";
+            result = "You lose! Scissors beats Paper";
             computer_score++;
         }
     } else if (human_choice === "scissors") {
         if (computer_choice === "rock") {
-            round_outcome.textContent = "You lose! Rock beats Scissors";
+            result = "You lose! Rock beats Scissors";
             computer_score++;
         } else if (computer_choice === "paper") {
-            round_outcome.textContent = "You win! Scissors beats Paper";
+            result = "You win! Scissors beats Paper";
             human_score++;
         } else {
-            round_outcome.textContent = "It's a draw!"
+            result = "It's a draw!";
         }
     }
 
-    running_score.textContent = `Your score: ${human_score}, Computer score: ${computer_score}`;
+    round_outcome.append(result);
+    running_score.textContent = `Your score: ${human_score}, Computer score: ${computer_score} — ${rounds_played} out of 5 rounds`;
+    
+    // Announce the results of the game after 5 rounds
+    // and reset the running score.
+    if (rounds_played === 5) {
+        game_outcome.textContent = "Game outcome: ";
+        if (human_score > computer_score) {
+            game_outcome.append("You won!");
+        } else if (human_choice < computer_score) {
+            game_outcome.append("You lost!");
+        } else {
+            game_outcome.append("It's a draw!");
+        }
+        rounds_played = 0;
+        human_score = 0;
+        computer_score = 0;
+    }
 }
